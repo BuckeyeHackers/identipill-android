4c03b35499e161bffdd2075177ca0f3a97047fed
emilyengle <emily.engle.97@gmail.com>
2016-11-05 17:00:18 -0400

    Minor adjustments to intro and settings pages

--- app/src/main/java/org/buckhacks/identipill/IntroActivity.java
+++ app/src/main/java/org/buckhacks/identipill/IntroActivity.java
@@ -1,7 +1,9 @@
 package org.buckhacks.identipill;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IntroActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class IntroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+    }
+
+
+    public void goToCamera (View view){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void goToSettings (View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/org/buckhacks/identipill/SettingsActivity.java
+++ app/src/main/java/org/buckhacks/identipill/SettingsActivity.java
@@ -1,7 +1,9 @@
 package org.buckhacks.identipill;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void goToCamera (View view){
+        Intent intent = new Intent(this, CameraActivity.class);
+        startActivity(intent);
     }
 }
